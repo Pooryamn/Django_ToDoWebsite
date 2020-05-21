a5d2cada63e04d5eb65927eb1a767b7edeba3957
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-21 15:20:09 +0430

    Add new Url to Sign Up User

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ToDoApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Auth 
+    path('signup/',views.SignUpUser,name='SignUpUser')
+
+    # Todos
 ]
