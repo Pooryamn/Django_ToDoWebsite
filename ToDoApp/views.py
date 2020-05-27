fcb3ffe3a3a5ad0f2189f02952f7c29eaae0b45e
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-28 01:18:52 +0430

    handle some errors in signup part of site

--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 # Create your views here.
 def SignUpUser(request):
+    
     if(request.method == "GET"):
         return render(request,'ToDoApp/SignUpUser.html',{'form': UserCreationForm()})
     else:
@@ -11,8 +14,13 @@ def SignUpUser(request):
 
         # Check if pass1 and pass2 is same
         if (request.POST['password1']==request.POST['password2']):
-            user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
-            user.save()
+            
+            try:
+                user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+                user.save()
+
+            except IntegrityError:
+                return render(request,'ToDoApp/SignUpUser.html',{'form': UserCreationForm(),'error':'that Username has already exists!'})     
 
         else:
             #tell user password isnt match
