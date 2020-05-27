0d18a3bee3dc7e8d4f78a67d8537bf0c8023640d
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-28 00:54:56 +0430

    check the type of request

--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def SignUpUser(request):
-
-    return render(request,'ToDoApp/SignUpUser.html',{'form': UserCreationForm()})
+    if(request.method == "GET"):
+        return render(request,'ToDoApp/SignUpUser.html',{'form': UserCreationForm()})
+    else:
+        # Create New user
